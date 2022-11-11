2d8dd5b58fad6a4728d0d5c41b7adb899d5a3c18
darkeyelids1337 <kamyzinaliliya161@gmail.com>
2022-11-11 19:49:01 +0400

    Peredelanno

--- src/script.js
+++ src/script.js
@@ -1,41 +1,40 @@
-const formButton = document.querySelector('.form-btn'),
-      city = document.querySelector('.city-form'),
-      input = document.querySelector('.input-form'),
-      wrapperMes = document.querySelector('.select-wrapper .error-mes'),
-      inputMes = document.querySelector('.input-error-mes'),
-      requestBtn = document.querySelector('.request-btn'),
-      form = document.querySelector('.form-section'),
-      fillingCourse = document.querySelector('.filling-course-panel'),
-      cityArrow = document.querySelector('.select-arrow')
+const formButton = document.querySelector('.form__button'),
+      city = document.querySelector('.form__city-form'),
+      input = document.querySelector('.form__input-form'),
+      wrapperMes = document.querySelector('.form__select-wrapper .form__error-mes'),
+      inputMes = document.querySelector('.form__input-error-mes'),
+      requestBtn = document.querySelector('.text-block__request-button'),
+      form = document.querySelector('.form'),
+      fillingCourse = document.querySelector('.filling-course__panel'),
+      cityArrow = document.querySelector('.form__arrow')
 
-wrapperMes.classList.add('visually-hidden');
-inputMes.classList.add('visually-hidden');
+wrapperMes.classList.add('_hidden');
+inputMes.classList.add('_hidden');
 
-formButton.addEventListener('click',function(evt){
-    evt.preventDefault();
-    city.value ? wrapperMes.classList.add('visually-hidden') : wrapperMes.classList.remove('visually-hidden');
-    input.value ? inputMes.classList.add('visually-hidden') : inputMes.classList.remove('visually-hidden');
-    wrapperMes.classList.contains('visually-hidden') ? city.style.backgroundColor = '#ECF1F7' : city.style.backgroundColor = 'rgba(224, 31, 25, 0.12)';
-    inputMes.classList.contains('visually-hidden') ? input.style.backgroundColor = '#ECF1F7' : input.style.backgroundColor = 'rgba(224, 31, 25, 0.12)';
+formButton.addEventListener('click',function(event){
+    event.preventDefault();
+    city.value ? wrapperMes.classList.add('_hidden') : wrapperMes.classList.remove('_hidden');
+    input.value ? inputMes.classList.add('_hidden') : inputMes.classList.remove('_hidden');
+    wrapperMes.classList.contains('_hidden') ? city.style.backgroundColor = '#ECF1F7' : city.style.backgroundColor = 'rgba(224, 31, 25, 0.12)';
+    inputMes.classList.contains('_hidden') ? input.style.backgroundColor = '#ECF1F7' : input.style.backgroundColor = 'rgba(224, 31, 25, 0.12)';
 });
 
-requestBtn.addEventListener('click', (evt) => {
-    evt.preventDefault();
+requestBtn.addEventListener('click', (event) => {
+    event.preventDefault();
     form.scrollIntoView();
 });
 
 city.addEventListener('focus',() => {
-    cityArrow.classList.toggle('toggle');
+    cityArrow.classList.toggle('_toggle');
 });
 city.addEventListener('focusout',() => {
-    cityArrow.classList.toggle('toggle');
+    cityArrow.classList.toggle('_toggle');
 });
-fillingCourse.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    const arrow = evt.target.parentNode.querySelector('.arrow');
-    const infoList = evt.target.parentNode.nextElementSibling.lastElementChild;
-    arrow.classList.toggle('toggle');
-    console.log(infoList);
-    infoList.classList.toggle('visible');
+fillingCourse.addEventListener('click', (event) => {
+    event.preventDefault();
+    const arrow = event.target.parentNode.querySelector('.filling-course__arrow');
+    const infoList = event.target.parentNode.nextElementSibling.lastElementChild;
+    arrow.classList.toggle('_toggle');
+    infoList.classList.toggle('_visible');
    
 });
